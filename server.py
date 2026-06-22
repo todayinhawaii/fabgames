0ea59e19a0f70cd80b58dccc922689df888afbee
todayinhawaii <zenrelaxationsanctuary@gmail.com>
2026-06-22 03:53:25 -1000

    Update server.py

--- server.py
+++ server.py
@@ -462,6 +462,10 @@ def fun_with_shapes():
 def magic_sound_keys():
     return send_from_directory('.', 'magic-sound-keys.html')
 
+@app.route('/street-hustler')
+def street_hustler():
+    return send_from_directory('.', 'street-hustler.html')
+
 @app.route('/freegames')
 def freegames():
     return send_from_directory('.', 'freegames.html')
